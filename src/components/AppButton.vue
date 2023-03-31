4009ccf030918f3f0e7ea9f49b31eeb20a79461f
Murad khan <murad.jaddoon@gmail.com>
2023-04-01 00:28:18 +0500

    created button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="handleClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+    name: "AppButton",
+    props: {
+        label: String,
+        handleClick: Function
+    }
+}
+</script>
+
+<style scoped>
+button {
+    background-color: yellowgreen;
+    border-radius: 8px;
+    padding: 10px;
+    font-weight: 600;
+    width: 100px;
+    border: 1px solid black;
+
+}
+</style>
