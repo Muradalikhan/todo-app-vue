771168c7c873c12867cdb274bb7b4d95afaa5e7a
Murad khan <murad.jaddoon@gmail.com>
2023-04-03 23:42:29 +0500

    created input component

---
+++ src/components/TextField.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <label v-if="label" :for="name">{{ label }}</label>
+        <input :id="name" :name="name" :placeholder="placeholder" :type="type" :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)" />
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "TextField",
+    props: {
+        label: {
+            type: [String, Boolean],
+            default: false,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        placeholder: {
+            type: String,
+        },
+        type: {
+            type: String,
+            default: "text",
+        },
+        modelValue: {
+            type: [String, Number],
+            default: "",
+        },
+    },
+};
+</script>
+  
